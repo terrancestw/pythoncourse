2bf614fc5d7a5d5c0bc080a6579223ed3d7f7b93
Terrance <terrancesim19@gmail.com>
2018-07-12 13:11:10 +0800

    adding health.py file

---
+++ health.py
@@ -0,0 +1,31 @@
+import random
+import math
+
+health = 50
+
+difficulty = 1
+
+potion_health = int(random.randint(25,50) / difficulty)
+
+health = health + potion_health
+
+print(health)
+
+print(math.floor(1.5))
+print(math.ceil(2.1))
+print(math.pi)
+print(math.e)
+print(math.sin(math.pi/2))
+print(math.sin(math.pi))
+print(math.floor(math.sin(math.pi)))
+print(math.cos(0))
+print(math.asin(0))
+print(math.acos(0))
+print(math.hypot(3,4))
+print(math.pow(2,3))
+print(2 ** 3)
+print(math.pow(9,2))
+print(math.exp(2))
+print(math.log(math.e))
+print(math.log10(1000))
+print(math.log2(8))
